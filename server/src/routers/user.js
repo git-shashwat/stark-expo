8eb56f568aef3f440268b81e07939f27a907b5ce
git-shashwat <shashwat824@gmail.com>
2020-06-27 23:01:19 +0530

    hope it works

--- server/src/routers/user.js
+++ server/src/routers/user.js
@@ -4,15 +4,16 @@ const auth = require('../middleware/auth');
 const router = new express.Router();
 
 router.post('/users/signup', async (req, res) => {
-    const user = new User(req.body);
+    res.send(req.body);
+    // const user = new User(req.body);
 
-    try {
-        await user.save();
-        const token = await user.generateAuthToken();
-        res.status(201).send({ user, token });
-    } catch (err) {
-        res.status(400).send(err);
-    }
+    // try {
+    //     await user.save();
+    //     const token = await user.generateAuthToken();
+    //     res.status(201).send({ user, token });
+    // } catch (err) {
+    //     res.status(400).send(err);
+    // }
 });
 
 router.post('/users/signin', async (req, res) => {

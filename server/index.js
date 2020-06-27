a76e19a74bb2f102c0e6481ceb7be37fee59f72b
git-shashwat <shashwat824@gmail.com>
2020-06-27 22:41:21 +0530

    fixing production

--- server/index.js
+++ server/index.js
@@ -22,14 +22,14 @@ app.use(express.json());
 
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
-app.use(userRouter);
-app.use(paymentRouter);
-app.use(collectionRouter);
-
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
 
+
+app.use(userRouter);
+app.use(paymentRouter);
+app.use(collectionRouter);
 
 app.listen(port, () => {
     console.log(`Server is up and running on port ${port}`);

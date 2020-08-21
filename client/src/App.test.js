6effd44a32c07aa6f24b879496c586187ea569b9
git-shashwat <shashwat824@gmail.com>
2020-08-21 15:39:44 +0530

    added test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
+import App from './routers/AppRouter';
+import { Provider } from 'react-redux';
+import { store, persistor } from './redux/store/configureStore';
+import { PersistGate } from 'redux-persist/integration/react';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const jsx = (
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
+  );
+
+  const { getByText } = render(jsx);
+  const linkElement = getByText(/CONTACT/i);
   expect(linkElement).toBeInTheDocument();
 });

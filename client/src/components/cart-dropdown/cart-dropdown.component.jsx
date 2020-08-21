f3050917a592d54108f69db39daa04c1b53574ac
git-shashwat <shashwat824@gmail.com>
2020-08-21 15:29:17 +0530

    node continuous integration

--- client/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ client/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {withRouter } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 import { createStructuredSelector } from 'reselect';
 import { connect } from 'react-redux';
 
